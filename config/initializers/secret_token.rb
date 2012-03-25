e8e5464ea1c87918eaf5b9d64410cf4dd705b709
W1lm3r <wanastacioy@gmail.com>
2012-03-25 13:47:11 -0500

    Primera carga

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LA12::Application.config.secret_token = '20526f7b78f0c78b6924f884ca24a6cb422609e83c82053b723d65686172f5fe7952af95056a948675c6a0dc8bf77e2b03def0310c977ba4dd63950a8727a423'
