1563687c0de9773e3e44b61d16e3ef31e250dbb1
pascua <a>
2012-04-05 13:36:35 +0200

    entidadesmodificaion

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120327051809) do
+ActiveRecord::Schema.define(:version => 20120327053433) do
 
   create_table "coments", :force => true do |t|
     t.string   "texto"
     t.integer  "calificacion"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "eventodepos", :force => true do |t|
+    t.string   "nombre"
+    t.decimal  "premio"
+    t.integer  "cantidad"
+    t.datetime "plazoinsc"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
