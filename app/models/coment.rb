7e2bf466cdd9b362750718d4a427dce3cbb382e0
W1lm3r <wanastacioy@gmail.com>
2012-04-14 11:07:39 -0500

    ultima Carga

--- app/models/coment.rb
+++ app/models/coment.rb
@@ -1,3 +1,7 @@
 class Coment < ActiveRecord::Base
 belongs_to :cancha
+	validates_presence_of :cancha_id, :message => "--Seleccione la cancha por favor"
+	validates_presence_of :texto, :message => "--Ingrese comentario  por favor"
+	validates_presence_of :calificacion, :message => "--Ingrese la calificacion por favor"
+	
 end
